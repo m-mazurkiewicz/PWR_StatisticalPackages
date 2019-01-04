6431e2da94df68aae1580c7d9ac323d422351f56
Marcin Mazurkiewicz <mmazurkiewicz95@gmail.com>
2019-01-04 21:37:22 +0100

    init report 2

---
+++ report2_code.R
@@ -0,0 +1,19 @@
+## TASK 1
+mu1 = 0
+mu2 = 0
+sample1 = rnorm(200, mu1, 2)
+sample2 = rnorm(200, mu2, 2)
+
+##TASK 2
+
+mu1 = 0
+mu2 = 0
+sample1 = rnorm(200, mu1, 2)
+sample2 = rnorm(200, mu2, 4)
+
+##TASK 3
+
+mu1 = 0
+mu2 = 0
+sample1 = rexp(200, 1/mu1)
+sample2 = rexp(200, 1/mu2)
