0c15f3da55169900a06c3abed55d25a93d7e2dce
Marcin Mazurkiewicz <mmazurkiewicz95@gmail.com>
2019-01-05 14:18:13 +0100

    task 1 completed

--- report2_code.R
+++ report2_code.R
@@ -1,19 +1,35 @@
+alpha = .05
+
+t.power1 = function(nsamp=c(200,200),nsim=1000,means=c(0,0),sds=c(1,1),var.equal=TRUE){
+  tps = replicate(nsim,
+                  t.test(rnorm(nsamp[1],mean=means[1],sd=sds[1]),
+                         rnorm(nsamp[2],mean=means[2],sd=sds[2]), var.equal = var.equal)$p.value)
+  
+  sum(tps < alpha/2 | tps > 1 - alpha/2) / nsim
+}
+
+wilcoxon.power = function(nsamp=c(200,200),nsim=1000,means=c(0,0),sds=c(1,1)){
+  tps = replicate(nsim,
+                  wilcox.test(rnorm(nsamp[1],mean=means[1],sd=sds[1]),
+                              rnorm(nsamp[2],mean=means[2],sd=sds[2]))$p.value)
+  
+  sum(tps < alpha/2 | tps > 1 - alpha/2) / nsim
+}
+
 ## TASK 1
-mu1 = 0
-mu2 = 0
-sample1 = rnorm(200, mu1, 2)
-sample2 = rnorm(200, mu2, 2)
+powers_student <- vector(mode="numeric", length=0)
+powers_welch <- vector(mode="numeric", length=0)
+powers_wilcoxon <- vector(mode="numeric", length=0)
+for (i in seq(-2,2,.1)){
+  powers_student = c(powers_student, t.power1(means = c(0, i), sds = c(2,2)))
+  powers_welch = c(powers_welch, t.power1(means = c(0, i), sds = c(2,2), var.equal = FALSE))
+  powers_wilcoxon = c(powers_wilcoxon, wilcoxon.power(means = c(0, i), sds = c(2,2)))
+}
 
-##TASK 2
+plot(seq(-2,2,.1), powers_student)
+plot(seq(-2,2,.1), powers_welch)
+plot(seq(-2,2,.1), powers_wilcoxon)
 
-mu1 = 0
-mu2 = 0
-sample1 = rnorm(200, mu1, 2)
-sample2 = rnorm(200, mu2, 4)
 
-##TASK 3
 
-mu1 = 0
-mu2 = 0
-sample1 = rexp(200, 1/mu1)
-sample2 = rexp(200, 1/mu2)
+
