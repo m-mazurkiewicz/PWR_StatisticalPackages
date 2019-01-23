d14f97b732ff550a73ee9e8ee76804362cf8d028
Marcin Mazurkiewicz <mmazurkiewicz95@gmail.com>
2019-01-23 23:10:42 +0100

    task 4 - without 3rd and 4th point

--- Report3/Report3-code.R
+++ Report3/Report3-code.R
@@ -1,13 +1,12 @@
-v = .7
-lambda <- 0.5
-T_0 <- 100
-
 # #https://stats.stackexchange.com/questions/148997/poisson-process-in-r-from-exponential-distribution
 # ## find the number 'n' of exponential r.vs required by imposing that
 # ## Pr{N(t) <= n} <= 1 - eps for a small 'eps'
 
 # TASK 2
 
+v = .7
+lambda <- 0.5
+T_0 <- 100
 n <- qpois(1 - 1e-8, lambda = v * T_0) # nie wiem czemu jest 1 - 1e-8
 X <- rexp(n = n, rate = v)
 inspection_times <- c(0, cumsum(X))
@@ -27,6 +26,7 @@ for(i in 1:(length(inspection_times) - 1)){
 
 plot(inspection_times)
 points(failureTimes, col = 'red')
+
 
 #TASK 3
 
@@ -56,3 +56,32 @@ for(j in 1:N){
 }
 
 avgNoOfReplacements = avgNoOfReplacements / N
+
+
+# task 4 - just as I thought - assume that failure is at the moment of inspection
+
+v = .7
+lambda <- 0.5
+T_0 <- 100
+n <- qpois(1 - 1e-8, lambda = v * T_0) # nie wiem czemu jest 1 - 1e-8
+X <- rexp(n = n, rate = v)
+inspection_times <- c(0, cumsum(X))
+
+lightbulb = 1
+failureTimes = c(0)
+lightbulbTime <- rexp(1, rate = lambda)
+lightbulbLifetimes <- c()
+for(i in 1:(length(inspection_times) - 1)){
+    if( inspection_times[i + 1] - failureTimes[lightbulb] > lightbulbTime){
+        failureTimes = append(failureTimes, inspection_times[i + 1])
+        lightbulb = lightbulb + 1
+        lightbulbLifetimes = append(lightbulbLifetimes, lightbulbTime)
+        lightbulbTime <- rexp(1, rate = lambda)
+    }
+}
+
+naive_lightbulb_lifetime = failureTimes[2:length(failureTimes)] - failureTimes[1:length(failureTimes) - 1]
+mean_naive = mean(naive_lightbulb_lifetime)
+variance_naive = var(naive_lightbulb_lifetime)
+
+
