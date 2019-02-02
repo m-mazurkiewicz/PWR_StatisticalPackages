f1b2cba172dcdba02fe85571f8e474f5747a8bf0
Marcin Mazurkiewicz <mmazurkiewicz95@gmail.com>
2019-02-02 14:52:11 +0100

    use litnr and change code. There are still some things that it says are wrong like too long line, but I wanted to leave them for now

--- Report3/Report3-code.R
+++ Report3/Report3-code.R
@@ -4,94 +4,94 @@
 
 # TASK 2
 
-v = .7
+v <- .7
 lambda <- 0.5
 T_0 <- 100
 # n <- qpois(1 - 1e-8, lambda = v * T_0) # nie wiem czemu jest 1 - 1e-8
 # X <- rexp(n = n, rate = v)
 # inspection_times <- c(0, cumsum(X))
 # inspections <- c(0,sort(runif(n = rpois(1, lambda=time.horizon*nu), min = 0, max = time.horizon)))
-inspection_times <- c(0,sort(runif(n = rpois(1, lambda=T_0*v), min = 0, max = T_0)))
+inspection_times <- c(0, sort(runif(n = rpois(1, lambda = T_0 * v), min = 0, max = T_0)))
 
-lightbulb = 1
-failureTimes = c(0)
+lightbulb <- 1
+failureTimes <- c(0)
 lightbulbTime <- rexp(1, rate = lambda)
 lightbulbLifetimes <- c()
-for(i in 1:(length(inspection_times) - 1)){
-  if( inspection_times[i + 1] - failureTimes[lightbulb] > lightbulbTime){
-    failureTimes = append(failureTimes, inspection_times[i + 1])
-    lightbulb = lightbulb + 1
-    lightbulbLifetimes = append(lightbulbLifetimes, lightbulbTime)
+for (i in 1:(length(inspection_times) - 1)){
+  if ( inspection_times[i + 1] - failureTimes[lightbulb] > lightbulbTime){
+    failureTimes <- append(failureTimes, inspection_times[i + 1])
+    lightbulb <- lightbulb + 1
+    lightbulbLifetimes <- append(lightbulbLifetimes, lightbulbTime)
     lightbulbTime <- rexp(1, rate = lambda)
   }
 }
 
 plot(inspection_times)
-points(failureTimes, col = 'red')
+points(failureTimes, col = "red")
 
 
 #TASK 3
 
-momentsOfFailure <- lightbulbLifetimes + failureTimes[1:length(failureTimes)-1]
-percentageOfTimeWithoutLight <- sum(failureTimes[2:length(failureTimes)] - momentsOfFailure) / tail(failureTimes, n=1)
+momentsOfFailure <- lightbulbLifetimes + failureTimes[1:length(failureTimes) - 1]
+percentage_of_time_withoutlight <- sum(failureTimes[2:length(failureTimes)] - momentsOfFailure) / tail(failureTimes, n = 1)
 
-N = 100
-avgNoOfReplacements = 0
-for(j in 1:N){
+N <- 100
+avg_no_of_replacements <- 0
+for (j in 1:N){
     n <- qpois(1 - 1e-8, lambda = v * T_0) # nie wiem czemu jest 1 - 1e-8
     X <- rexp(n = n, rate = v)
     inspection_times <- c(0, cumsum(X))
 
-    lightbulb = 1
-    failureTimes = c(0)
+    lightbulb <- 1
+    failureTimes <- c(0)
     lightbulbTime <- rexp(1, rate = lambda)
     lightbulbLifetimes <- c()
-    for(i in 1:(length(inspection_times) - 1)){
-        if( inspection_times[i + 1] - failureTimes[lightbulb] > lightbulbTime){
-            failureTimes = append(failureTimes, inspection_times[i + 1])
-            lightbulb = lightbulb + 1
-            lightbulbLifetimes = append(lightbulbLifetimes, lightbulbTime)
+    for (i in 1:(length(inspection_times) - 1)){
+        if ( inspection_times[i + 1] - failureTimes[lightbulb] > lightbulbTime){
+            failureTimes <- append(failureTimes, inspection_times[i + 1])
+            lightbulb <- lightbulb + 1
+            lightbulbLifetimes <- append(lightbulbLifetimes, lightbulbTime)
             lightbulbTime <- rexp(1, rate = lambda)
         }
     }
-    avgNoOfReplacements = avgNoOfReplacements + lightbulb
+    avg_no_of_replacements <- avg_no_of_replacements + lightbulb
 }
 
-avgNoOfReplacements = avgNoOfReplacements / N
+avg_no_of_replacements <- avg_no_of_replacements / N
 
 
 # task 4 - just as I thought - assume that failure is at the moment of inspection
 
-v = .7
+v <- .7
 lambda <- 0.5
 T_0 <- 100
-inspection_times <- c(0,sort(runif(n = rpois(1, lambda=T_0*v), min = 0, max = T_0)))
+inspection_times <- c(0, sort(runif(n = rpois(1, lambda = T_0 * v), min = 0, max = T_0)))
 
-lightbulb = 1
-failureTimes = c(0)
+lightbulb <- 1
+failureTimes <- c(0)
 lightbulbTime <- rexp(1, rate = lambda)
 lightbulbLifetimes <- c()
-for(i in 1:(length(inspection_times) - 1)){
-    if( inspection_times[i + 1] - failureTimes[lightbulb] > lightbulbTime){
-        failureTimes = append(failureTimes, inspection_times[i + 1])
-        lightbulb = lightbulb + 1
-        lightbulbLifetimes = append(lightbulbLifetimes, lightbulbTime)
+for (i in 1:(length(inspection_times) - 1)){
+    if ( inspection_times[i + 1] - failureTimes[lightbulb] > lightbulbTime){
+        failureTimes <- append(failureTimes, inspection_times[i + 1])
+        lightbulb <- lightbulb + 1
+        lightbulbLifetimes <- append(lightbulbLifetimes, lightbulbTime)
         lightbulbTime <- rexp(1, rate = lambda)
     }
 }
 
-naive_lightbulb_lifetime = failureTimes[2:length(failureTimes)] - failureTimes[1:length(failureTimes) - 1]
-mean_naive = mean(naive_lightbulb_lifetime)
-variance_naive = var(naive_lightbulb_lifetime)
+naive_lightbulb_lifetime <- failureTimes[2:length(failureTimes)] - failureTimes[1:length(failureTimes) - 1]
+mean_naive <- mean(naive_lightbulb_lifetime)
+variance_naive <- var(naive_lightbulb_lifetime)
 
 
 # task 5 - this estimator assumes that between last inspection and moment of bulb replacement (next inspection) the probability of light failure comes from exponential or uniform distribution
 
-naive_lightbulb_lifetime = failureTimes[2:length(failureTimes)] - failureTimes[1:length(failureTimes) - 1]
-uniform_multiplier = runif(length(naive_lightbulb_lifetime))
-uniform_lightbulb_lifetime = uniform_multiplier * naive_lightbulb_lifetime
-mean_uniform = mean(uniform_lightbulb_lifetime)
-variance_uniform = var(uniform_lightbulb_lifetime)
+naive_lightbulb_lifetime <- failureTimes[2:length(failureTimes)] - failureTimes[1:length(failureTimes) - 1]
+uniform_multiplier <- runif(length(naive_lightbulb_lifetime))
+uniform_lightbulb_lifetime <- uniform_multiplier * naive_lightbulb_lifetime
+mean_uniform <- mean(uniform_lightbulb_lifetime)
+variance_uniform <- var(uniform_lightbulb_lifetime)
 
 #for exponential case basically in the same way, but different multiplier is needed (what multipler??)
 # naive_lightbulb_lifetime = failureTimes[2:length(failureTimes)] - failureTimes[1:length(failureTimes) - 1]
